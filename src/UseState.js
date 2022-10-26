dc7cfa14b6d1acb28a655e59a8cdc678727b22da
Erick <erickcampaz@gmail.com>
2022-10-26 18:02:44 -0500

    Estados compuestos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -5,23 +5,38 @@ const SECURITY_CODE='paradigma'; //estos es para validar el cidigo se seguridad
 
 
  function UseState({name}){// si cambaimos el nombre a Usestate en App, cambia aqui tambien name
+    const[state, setState]= React.useState({ //Para guardar los estados
+        value:'',
+        error:false,
+        loading:false,
+    });
     const [value, setvalue]= React.useState('');// los usario escriba
     const [error, setError]= React.useState(false);
     const [loading, setloading]= React.useState(false); //son estado se de poner asi por convecion se lla state, setState
 
-    console.log(value);
+    console.log(state);
 
     React.useEffect(()=> {
         console.log("empemzando el efecto")
         
-        if(!!loading){
+        if(!!state.loading){
             setTimeout(() => {
                 console.log("Haciendo la validacion")
 
-                if (value !== SECURITY_CODE) {
-                  setError(true);  
-                }
-               setloading(false);
+                if (state.value === SECURITY_CODE) {
+                    setState({
+                        ...state,
+                        error:false,
+                        loading: false,
+                    });
+              /*  setloading(false); */
+            }else{
+                setState({
+                    ...state,
+                    error:true,
+                    loading: false,
+                });
+            }
             
           
 //estos para que espere para que se juecute la aplicacion
@@ -31,7 +46,7 @@ const SECURITY_CODE='paradigma'; //estos es para validar el cidigo se seguridad
         }
 
         console.log("Terminado el efecto")
-        }, [loading]);
+        }, [state.loading]);
 
       
 
@@ -41,25 +56,32 @@ const SECURITY_CODE='paradigma'; //estos es para validar el cidigo se seguridad
         <h2>Eliminar {name}</h2>
         <p>Por favor escribe el codigo de seguridad</p>
         
-        {(error && !loading ) && ( /* se le poso el loading para cuando vamos a validar el estado se quite el texto de codigo incorrecto */
+        {(state.error && !state.loading ) && ( /* se le poso el loading para cuando vamos a validar el estado se quite el texto de codigo incorrecto */
             <p>Erro: de codigo es incorreto</p>
         )} {/* esta pregunto si erro es tru renderiseme lo que sepone  despues de &&, si cambie arriba de donde false a tru sale el texto */}
     
-        {loading && (
+        {state.loading && (
             <p>Cargando...</p>
         )}  
         <input 
         placeholder="Codigo de seguridad"
-        value={value}
+        value={state.value}
         onChange={(event)=>{
+            setState({
+                ...state,
+                value: event.target.value,
+            });
             /* setError(false); */ // este para que cambie cuando el usuario esta escribiendo el estado de error
-            setvalue(event.target.value); //el valor sera lo que escriba los usuario
+            /* setvalue(event.target.value); //el valor sera lo que escriba los usuario */
         }}
         />
         <button
         onClick={() => {
         /* setError(false); */ //para que cambie el estado de carga si el codigo es correcto se quita
-        setloading(true);
+        setState({
+            ...state,
+            loading: true,
+        });
     }} 
     > Comprobando</button>
         
